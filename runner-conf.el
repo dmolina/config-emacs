bdea62772bb0842d2a6fbbcbbaf373e404f61c26
Daniel Molina <daniel.molina@uca.es>
2016-06-27 17:49:10 +0200

    Feature: Add svg format to runner.

--- runner-conf.el
+++ runner-conf.el
@@ -1,4 +1,4 @@
 
-("ext-ods" "ext-movie" "ext-pdf")
+("ext-svg" "ext-ods" "ext-movie" "ext-pdf")
 
-(("ext-ods" 0 "ods xls xlsx" (("libreoffice" "soffice &" 5))) ("ext-movie" 0 "mp4 part rm wmv mov" (("mpv" "mpv &" 0))) ("ext-pdf" 0 "pdf" (("evince" "evince &" 0))))
+(("ext-svg" 0 "svg" (("inkscape" "inkscape &" 5))) ("ext-ods" 0 "ods xls xlsx" (("libreoffice" "soffice &" 5))) ("ext-movie" 0 "mp4 part rm wmv mov" (("mpv" "mpv &" 0))) ("ext-pdf" 0 "pdf" (("evince" "evince &" 0))))
